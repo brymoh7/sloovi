f66dba5443941eb120e322be229ce71f375a3bdf
Tolu brymoh <83027769+brymoh7@users.noreply.github.com>
2022-08-25 18:08:11 +0100

    Delete src directory

--- src/state/slices/constants.js
+++
@@ -1,7 +0,0 @@
-export const REQUEST_STATUS = Object.freeze({
-    PENDING: 'PENDING',
-    FULFILLED: 'FULFILLED',
-    REJECTED: 'REJECTED',
-    NOT_VERIFIED: 'NOT_VERIFIED',
-    VERIFIED: 'VERIFIED',
-});
